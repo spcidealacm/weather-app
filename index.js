aa26f7b4de2f2716aeda5bea19a3de297dcff809
spcidealacm <spcidealacm@gmail.com>
2021-07-30 16:56:12 +1000

    heroku create

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const http = require("http");
 const https = require("https");
 const fs = require("fs");
 
-const options = {
-    key: fs.readFileSync("./src/config/aitopcoder-com.key"), // https ssl key
-    cert: fs.readFileSync("./src/config/aitopcoder-com.crt"), // https ssl crt
-};
+// const options = {
+//     key: fs.readFileSync("./src/config/aitopcoder-com.key"), // https ssl key
+//     cert: fs.readFileSync("./src/config/aitopcoder-com.crt"), // https ssl crt
+// };
 
 app.use(async (ctx, next) => {
-    await next();
+  await next();
 });
 
 app.use(cors());
@@ -20,10 +20,10 @@ app.use(cors());
 app.use(router.routes());
 
 getPort().then((port) => {
-    http.createServer(app.callback()).listen(port);
-    console.log("http server port: ", port);
-    getPort().then((port) => {
-        https.createServer(options, app.callback()).listen(port);
-        console.log("https server port: ", port);
-    });
+  http.createServer(app.callback()).listen(port);
+  console.log("http server port: ", port);
+  // getPort().then((port) => {
+  //     https.createServer(options, app.callback()).listen(port);
+  //     console.log("https server port: ", port);
+  // });
 });
